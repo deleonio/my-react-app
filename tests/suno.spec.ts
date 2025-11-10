1aad584e9fbc65938c8ed014b8bc717cf12d5cde
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-11-11 00:45:37 +0100

    test: add additional song hash to Suno Song Tests

--- tests/suno.spec.ts
+++ tests/suno.spec.ts
@@ -9,7 +9,7 @@ test.use({
 
 test.describe('Suno Song Tests', () => {
 	test.describe.configure({ mode: 'parallel' });
-	['066077da-ae8e-40f1-a51d-64bdd3c89668', '4d05d147-088c-4e03-88a2-5535cb4665b9'].forEach((hash) => {
+	['066077da-ae8e-40f1-a51d-64bdd3c89668', '4d05d147-088c-4e03-88a2-5535cb4665b9', '5414713c-1422-4b59-9187-791f9ff1c3b5'].forEach((hash) => {
 		test(`Test ${hash}`, async ({ page }) => {
 			await page.goto(`https://suno.com/song/${hash}`);
 			await page.getByRole('button', { name: 'Playbar: Play button' }).click();
