f04770f162ea2f2f3a1e10a6c09e26dc7edab840
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2025-12-24 06:18:18 +0100

    Refactor Suno Song Tests to remove URLs from comments and clean up song identifiers

--- tests/suno.spec.ts
+++ tests/suno.spec.ts
@@ -12,16 +12,16 @@ test.describe('Suno Song Tests', () => {
 		mode: 'parallel',
 	});
 	[
-		'066077da-ae8e-40f1-a51d-64bdd3c89668', // https://suno.com/song/066077da-ae8e-40f1-a51d-64bdd3c89668 (The Little Hummingbird)
-		'4d05d147-088c-4e03-88a2-5535cb4665b9', // https://suno.com/song/4d05d147-088c-4e03-88a2-5535cb4665b9 (Kleiner KoliBri flieg)
-		'5414713c-1422-4b59-9187-791f9ff1c3b5', // https://suno.com/song/5414713c-1422-4b59-9187-791f9ff1c3b5 (Hummingbird fly)
-		'98662b90-21a3-4adc-8244-8e0f17e6ae9f', // https://suno.com/song/98662b90-21a3-4adc-8244-8e0f17e6ae9f (Hummingbird fly - Psychedelic electro)
-		'2ef33e4e-0761-4702-b2d2-4487db559c22', // https://suno.com/song/2ef33e4e-0761-4702-b2d2-4487db559c22 (Hummingbird fly - Hardcode)
-		'a9dabf08-c38c-4025-a298-2215c4d08358', // https://suno.com/song/a9dabf08-c38c-4025-a298-2215c4d08358 (Hummingbird fly - Uplifting indie pop)
-		'6c5c2ced-e9b9-459a-b424-e84cd6fe6b6b', // https://suno.com/song/6c5c2ced-e9b9-459a-b424-e84cd6fe6b6b (Hummingbird fly - Classic)
-		'60cfc43d-3766-4e54-9506-3a5e9e542dd2', // https://suno.com/song/60cfc43d-3766-4e54-9506-3a5e9e542dd2 (Hummingbird fly - Opera)
-		'a4a985c3-4c2e-4fb5-b80d-0b43a3691bb9', // https://suno.com/song/a4a985c3-4c2e-4fb5-b80d-0b43a3691bb9 (Hummingbird fly - Madball 1)
-		'0bd693c9-8261-459b-a298-8888d570718c', // https://suno.com/song/0bd693c9-8261-459b-a298-8888d570718c (Hummingbird fly - Madball 2)
+		'066077da-ae8e-40f1-a51d-64bdd3c89668', // The Little Hummingbird
+		'4d05d147-088c-4e03-88a2-5535cb4665b9', // Kleiner KoliBri flieg
+		'5414713c-1422-4b59-9187-791f9ff1c3b5', // Hummingbird fly
+		'98662b90-21a3-4adc-8244-8e0f17e6ae9f', // Hummingbird fly (Psychedelic electro)
+		'a9dabf08-c38c-4025-a298-2215c4d08358', // Hummingbird fly (Uplifting indie pop)
+		'0bd693c9-8261-459b-a298-8888d570718c', // Hummingbird fly (Madball)
+		'b0eb435d-19b9-48cb-8e47-bb85efb6cff0', // Hummingbird fly (Melodic Techno)
+		'df883ab7-454a-45cf-8047-4bb23513843f', // Hummingbird fly (Orchestral)
+		'29b9708b-c19e-4809-b02c-d41ba9a84aed', // Hummingbird fly (Dance-Punk)
+		'c0dcb245-9a92-4e7b-8a7a-7e02a9bdc611', // Hummingbird fly (Pop-Rock)
 	].forEach((hash) => {
 		test(`Test ${hash}`, async ({ page }) => {
 			await page.goto(`https://suno.com/song/${hash}`);
